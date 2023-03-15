567022cec62211d2dc6c5a99d1c90159d1c02059
Jian Zhong <zhongjian089@gmail.com>
2023-03-15 05:42:18 -0700

    Project 8 started 3

---
+++ project8/compiled/node_crypto.bundle.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkproject6"] = self["webpackChunkproject6"] || []).push([["node_crypto"],{
+
+/***/ "node:crypto":
+/*!*******************!*\
+  !*** node:crypto ***!
+  \*******************/
+/***/ (() => {
+
+throw new Error("Module build failed: UnhandledSchemeError: Reading from \"node:crypto\" is not handled by plugins (Unhandled scheme).\nWebpack supports \"data:\" and \"file:\" URIs by default.\nYou may need an additional plugin to handle \"node:\" URIs.\n    at /Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/webpack/lib/NormalModule.js:832:25\n    at Hook.eval [as callAsync] (eval at create (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:6:1)\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/tapable/lib/Hook.js:18:14)\n    at Object.processResource (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/webpack/lib/NormalModule.js:829:8)\n    at processResource (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/loader-runner/lib/LoaderRunner.js:220:11)\n    at iteratePitchingLoaders (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/loader-runner/lib/LoaderRunner.js:171:10)\n    at runLoaders (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/loader-runner/lib/LoaderRunner.js:398:2)\n    at NormalModule._doBuild (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/webpack/lib/NormalModule.js:819:3)\n    at NormalModule.build (/Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/webpack/lib/NormalModule.js:963:15)\n    at /Users/zhongjian/Desktop/Stanford-CS142/project7ExtraCredit/node_modules/webpack/lib/Compilation.js:1371:12");
+
+/***/ })
+
+}]);
---
+++ project8/components/userDetail/userDetail.jsx
@@ -0,0 +1,102 @@
+import React from "react";
+import { Link, Redirect } from "react-router-dom";
+import { Button, Grid, Typography } from "@material-ui/core";
+import "./userDetail.css";
+import axios from "axios";
+
+
+/**
+ * * Jian Zhong
+ * Define UserDetail, a React componment of CS142 project #5
+ */
+export default class UserDetail extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null, // to receive user detail data from server
+    };
+  }
+
+  // Use Axios to send request and update the user state variable.
+  axios_fetchData(url) {
+    axios
+      .get(url)
+      .then(response => {
+        this.props.onUserNameChange( response.data.first_name + " " + response.data.last_name ); // handle TopBar user name change
+        this.props.onLoginUserChange({          // handle page refresh
+          first_name: response.data.first_name,
+          _id: response.data._id,
+        });
+        this.setState({ user: response.data }); // to display user detail data
+        console.log("** UserDetail: fetched user detail **");
+      })
+      .catch(error => {     // Handle error:
+        console.log("** Error in UserDetail **\n", error.message);
+      });
+  }
+  
+
+  // load data when page first load or refresh the page
+  componentDidMount() {
+    this.axios_fetchData(`/user/${this.props.match.params.userId}`);
+  }
+
+  /**
+   * load data user click on different user list and show the user's detail
+   * * component is not re-rendering when the route changes, componentDidUpdate() can detect route changes.
+   */
+  componentDidUpdate(prevProps) {
+    const prevUserID = prevProps.match.params.userId;
+    const currUserID = this.props.match.params.userId;
+    if (prevUserID !== currUserID && currUserID) {
+      this.axios_fetchData(`/user/${currUserID}`);
+    }
+  }
+
+  render() {
+    // * Note: need to add "|| !this.state.user" so that after 
+    // * redirecting to another page, this.state.user
+    // * won't be accessed on another page,
+    // * else it will cause unmount error in browser's console 
+    if (this.props.loginUser || !this.state.user) {
+      return this.state.user && (
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography color="textSecondary">Name:</Typography>
+            <Typography variant="h6" gutterBottom>
+              {`${this.state.user.first_name} ${this.state.user.last_name}`}
+            </Typography>
+            <Typography color="textSecondary">Description:</Typography>
+            <Typography variant="h6" gutterBottom>
+              {`${this.state.user.description}`}
+            </Typography>
+            <Typography color="textSecondary">Location:</Typography>
+            <Typography variant="h6" gutterBottom>
+              {`${this.state.user.location}`}
+            </Typography>
+            <Typography color="textSecondary">Occupation:</Typography>
+            <Typography variant="h6" gutterBottom>
+              {`${this.state.user.occupation}`}
+            </Typography>
+          </Grid>
+          <Grid item xs={4} />
+          <Grid item xs={4}>
+            <Button
+              size="large"
+              to={this.state.user && `/photos/${this.state.user._id}`}
+              component={Link}
+              variant="contained"
+              color="primary"
+            >
+              See Photos
+            </Button>
+          </Grid>
+          <Grid item xs={4} />
+        </Grid>
+      );
+    } else {
+      return <Redirect to={`/login-register`} />;
+    }
+
+  }
+}
