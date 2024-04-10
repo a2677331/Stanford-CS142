3e19cc2dc6fefd3c6d82542dee258ef7c846c0ca
Jian Zhong <zhongjian089@gmail.com>
2024-04-10 06:08:25 -0700

    converted loginRegister.jsx into functional component

--- project8/components/loginRegister/loginRegister.jsx
+++ project8/components/loginRegister/loginRegister.jsx
@@ -1,231 +1,188 @@
-import * as React from "react";
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
-import axios from "axios";
-import { Typography, Grid, FormControl, InputLabel, Input, Button } from "@material-ui/core";
+import axios from 'axios';
+import { Typography, Grid, FormControl, InputLabel, Input, Button } from '@material-ui/core';
 
-/**
- * * Jian Zhong
- * * Project 7 codes for implementing LoginRegister component
- */
+function LoginRegister({ loginUser, onLoginUserChange }) {
+  const [state, setState] = useState({
+    loginId: '',
+    loginName: '',
+    password: '',
+    loginMessage: '',
+    newLoginName: '',
+    firstName: '',
+    lastName: '',
+    description: '',
+    location: '',
+    occupation: '',
+    newPassword: '',
+    newPassword2: '',
+    registeredMessage: '',
+  });
 
+  const handleInputChange = ({ target }) => {
+    setState({ ...state, [target.name]: target.value });
+  };
 
-class LoginRegister extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loginId: "",
-      loginName: "",
-      password: "",
-      loginMessage: "", // use for prompting login info under login submit button
-      newLoginName: "",
-      firstName: "",
-      lastName: "",
-      description: "",
-      location: "",
-      occupation: "",
-      newPassword: "",
-      newPassword2: "",
-      registeredMessage: "", // use for prompting register info under register submit button
-    };
-    this.isLogged = null; // use for prompting user when login fails
-  }
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    console.log('** Sending login Info to server: ', state.loginName + '  ' + state.password + ' **');
 
-
-  // Handle change of login name and password
-  handleInputChange = ({ target }) => this.setState({ [target.name]: target.value });
-
-
-  /**
-  * Handle existing user login form sumbit
-  * use Axios to send POST request with both login name and password to server
-  * * always use Axios to send request and update the users state variable
-  * */ 
-  handleLoginSubmit = e => { // async: to use "await" inside the func
-    e.preventDefault();  // prevent submit form and reload page behavior.
-    console.log("** Sending login Info to server: ", this.state.loginName + "  " + this.state.password + " **");
-
-    // construct a login user obj
-    const loginUser = {         
-      login_name: this.state.loginName,
-      passwordClearText: this.state.password,
-    };  
-    
-    // axios to send a POST request with login info
     axios
-      .post("/admin/login", loginUser) // POST request sent!
-      .then(response => {   // Handle success
-        console.log(`** LoginRegister: loggin Success! **`); 
-        console.log("Printing response.data: ", response.data);
-        this.setState({ loginId: response.data.id});
-        this.props.onLoginUserChange(response.data);  // { first_name: , id: }, Passing back loggin user data and logged info to TopBar
+      .post('/admin/login', {
+        login_name: state.loginName,
+        passwordClearText: state.password,
       })
-      .catch(error => {     // Handle error
-        console.log(`** LoginRegister: loggin Fail! **`); 
-        this.setState({ loginMessage: error.response.data.message });
-        this.props.onLoginUserChange(null);  // Pass back info to TopBar
+      .then((response) => {
+        console.log('** LoginRegister: loggin Success! **');
+        console.log('Printing response.data: ', response.data);
+        setState({ ...state, loginId: response.data.id }); // if loginId is presented, then login page knows user is logged in.
+        onLoginUserChange(response.data);
+      })
+      .catch((error) => {
+        console.log('** LoginRegister: loggin Fail! **');
+        setState({ ...state, loginMessage: error.response.data.message });
+        onLoginUserChange(null);
       });
   };
 
+  // assign registered user values and reset user fillings
+  const getNewUser = () => {
+    const newUser = {
+      login_name: state.newLoginName,
+      passwordClearText: state.newPassword,
+      first_name: state.firstName,
+      last_name: state.lastName,
+      location: state.location,
+      description: state.description,
+      occupation: state.occupation,
+    };
 
-  /**
-  * Handle new user creation form sumbit
-  * use Axios to send POST request to server to register a new user
-  * */ 
-  handleRegisterSubmit = e => {
+    setState({
+      ...state,
+      newLoginName: '',
+      newPassword: '',
+      newPassword2: '',
+      firstName: '',
+      lastName: '',
+      location: '',
+      description: '',
+      occupation: '',
+    });
+
+    return newUser;
+  };
+
+
+  const handleRegisterSubmit = (e) => {
     e.preventDefault();
 
-    // make sure the two passwords entered are the same
-    if (this.state.newPassword !== this.state.newPassword2) {
-      this.setState({ registeredMessage: "The two passwords are NOT the same, please try again" }); // update message prompt to indicate user
+    if (state.newPassword !== state.newPassword2) {
+      setState({ ...state, registeredMessage: 'The two passwords are NOT the same, please try again' });
       return;
     }
 
-    // axios to send a POST request with login info
     axios
-      .post("/user", this.getNewUser()) // POST request sent!
-      .then(response => {   // Handle success
-        console.log(`** LoginRegister: new User register Success! **`); 
-        this.setState({ registeredMessage: response.data.message }); // update message prompt to indicate user
+      .post('/user', getNewUser())
+      .then((response) => {
+        console.log('** LoginRegister: new User register Success! **');
+        setState({ ...state, registeredMessage: response.data.message });
       })
-      .catch(error => {     // Handle error
-        console.log(`** LoginRegister: new User loggin Fail! **`); 
-        this.setState({ registeredMessage: error.response.data.message }); // update error prompt to indicate user
+      .catch((error) => {
+        console.log('** LoginRegister: new User loggin Fail! **');
+        setState({ ...state, registeredMessage: error.response.data.message });
       });
   };
-  
-
-  /**
-   * To construct new user obj and clear old inputs.
-   * @returns a new user object for sending to server
-   */
-  getNewUser() {
-
-    // construct a new user obj
-    const newUser = {
-      login_name: this.state.newLoginName, 
-      passwordClearText: this.state.newPassword,
-      first_name: this.state.firstName,
-      last_name: this.state.lastName,
-      location: this.state.location,
-      description: this.state.description,
-      occupation: this.state.occupation
-    };  
-    
-    // clear register inputs
-    this.setState({ 
-      newLoginName: "", 
-      newPassword: "",
-      newPassword2: "",
-      firstName: "",
-      lastName: "",
-      location: "",
-      description: "",
-      occupation: "",
-    });
-    
-    return newUser;
-  }
 
 
-  customForm(inputLabel, id, type, value, required, autoFocus = false) {
-     return (
-       <FormControl fullWidth>
-         <InputLabel htmlFor={id}>{inputLabel}</InputLabel>
-         <Input
-           name={id}
-           id={id}
-           autoFocus={autoFocus}
-           autoComplete="on"
-           type={type}
-           value={value}
-           onChange={this.handleInputChange}
-           required={required}
-         />
-       </FormControl>
-     );
-  }
-  
 
-  render() {
-    // Handle jumping into the login User's detail page if user is authorizedx
-    if (this.props.loginUser) {
-      return <Redirect from="/login-register" to={`/users/${this.state.loginId}`} />;
-       // the state prop in <Redirect> is used to pass data between components when using client-side routing
-    }
-
-    // else, show login page:
+  const customForm = (inputLabel, id, type, value, required, autoFocus = false) => {
     return (
-      <Grid container >
-
-        {/* Login Form */}
-        <Grid container item direction="column" alignItems="center" xs={6}>
-          {/* Welcome prompt */}
-          <Typography variant="h5">Log In</Typography>
-          {/* login inputs */}
-          <Grid item xs={8}>
-            <form onSubmit={this.handleLoginSubmit}>
-              {this.customForm("Login Name", "loginName", "text", this.state.loginName, true, true)}
-              {this.customForm("Password", "password", "password", this.state.password, true)}
-              <br/><br/>
-              <Button
-                type="submit"
-                disabled={this.state.loginName.length === 0}
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Login
-              </Button>
-              <br/><br/>
-              {this.state.loginMessage && (
-                <Typography style={{ color: "red" }}>
-                  {this.state.loginMessage}
-                </Typography>
-              )}
-            </form>
-          </Grid>
-        </Grid>
-
-        {/* Register Form */}
-        <Grid container item direction="column" alignItems="center" xs={6}>
-          {/* Welcome prompt */}
-          <Typography variant="h5">Create New Account</Typography>
-          {/* Register inputs */}
-          <Grid item xs={8}>
-            <form onSubmit={this.handleRegisterSubmit}>
-              {this.customForm("Login Name*", "newLoginName", "text", this.state.newLoginName, true)}
-              {this.customForm("First Name*", "firstName", "text", this.state.firstName, true)}
-              {this.customForm("Last Name*", "lastName", "text", this.state.lastName, true)}
-              {this.customForm("New Password*", "newPassword", "password", this.state.newPassword, true)}
-              {this.customForm("Verify Password*", "newPassword2", "password", this.state.newPassword2, true)}
-              {this.customForm("Description", "description", "text", this.state.description)}
-              {this.customForm("Location", "location", "text", this.state.location)}
-              {this.customForm("Occupation", "occupation", "text", this.state.occupation)}
-              <br/><br/>
-              <Button
-                type="submit"
-                disabled={this.state.newLoginName.length === 0}
-                fullWidth
-                variant="contained"
-                color="primary"
-              >
-                Register Me!
-              </Button>
-              <br/><br/>
-              {this.state.registeredMessage && (
-                <Typography style={{ color: this.state.registeredMessage.includes("successfully") ? "green" : "red" }}>
-                  {this.state.registeredMessage}
-                </Typography>
-              )}
-
-              {/* The two passwords are not the same prompt */}
-            </form>
-          </Grid>
-        </Grid>
-
-      </Grid>
+      <FormControl fullWidth>
+        <InputLabel htmlFor={id}>{inputLabel}</InputLabel>
+        <Input
+          name={id}
+          id={id}
+          autoFocus={autoFocus}
+          autoComplete="on"
+          type={type}
+          value={value}
+          onChange={handleInputChange}
+          required={required}
+        />
+      </FormControl>
     );
+  };
+
+  if (loginUser) {
+    return <Redirect from="/login-register" to={`/users/${state.loginId}`} />;
   }
+
+  return (
+    <Grid container>
+      {/* Login Form */}
+      <Grid container item direction="column" alignItems="center" xs={6}>
+        <Typography variant="h5">Log In</Typography>
+        {/* login inputs */}
+        <Grid item xs={8}>
+          <form onSubmit={handleLoginSubmit}>
+            {customForm('Login Name', 'loginName', 'text', state.loginName, true, true)}
+            {customForm('Password', 'password', 'password', state.password, true)}
+            <br />
+            <br />
+            <Button
+              type="submit"
+              disabled={state.loginName.length === 0}
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Login
+            </Button>
+            <br />
+            <br />
+            {state.loginMessage && <Typography style={{ color: 'red' }}>{state.loginMessage}</Typography>}
+          </form>
+        </Grid>
+      </Grid>
+
+      {/* Register Form */}
+      <Grid container item direction="column" alignItems="center" xs={6}>
+        <Typography variant="h5">Create New Account</Typography>
+        {/* Register inputs */}
+        <Grid item xs={8}>
+          <form onSubmit={handleRegisterSubmit}>
+            {customForm('Login Name*', 'newLoginName', 'text', state.newLoginName, true)}
+            {customForm('First Name*', 'firstName', 'text', state.firstName, true)}
+            {customForm('Last Name*', 'lastName', 'text', state.lastName, true)}
+            {customForm('New Password*', 'newPassword', 'password', state.newPassword, true)}
+            {customForm('Verify Password*', 'newPassword2', 'password', state.newPassword2, true)}
+            {customForm('Description', 'description', 'text', state.description)}
+            {customForm('Location', 'location', 'text', state.location)}
+            {customForm('Occupation', 'occupation', 'text', state.occupation)}
+            <br />
+            <br />
+            <Button
+              type="submit"
+              disabled={state.newLoginName.length === 0}
+              fullWidth
+              variant="contained"
+              color="primary"
+            >
+              Register Me!
+            </Button>
+            <br />
+            <br />
+            {state.registeredMessage && (
+              <Typography style={{ color: state.registeredMessage.includes('successfully') ? 'green' : 'red' }}>
+                {state.registeredMessage}
+              </Typography>
+            )}
+          </form>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default LoginRegister;
